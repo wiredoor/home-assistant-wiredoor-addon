9fea627da17ca7459e03840cec672b7ff2929781
Daniel Mesa <94721235+dmesad@users.noreply.github.com>
2025-05-27 10:17:21 -0400

    Update run.sh

--- wiredoor/run.sh
+++ wiredoor/run.sh
@@ -3,15 +3,14 @@ set -e
 
 TOKEN=$(jq -r '.token' /data/options.json)
 WIREDOOR_URL=$(jq -r '.url' /data/options.json)
-# GATEWAY_SUBNET=$(nslookup homeassistant | awk '/^Address: / { print $2; exit }')/32
 
 if [[ -z "$TOKEN" || "$TOKEN" == "null" ]]; then
-  echo "[ERROR] Token is not set. Please define it in the add-on options."
+  echo "[ERROR] TOKEN is not set. Please define it in the add-on options."
   exit 1
 fi
 
 if [[ -z "$WIREDOOR_URL" || "$WIREDOOR_URL" == "null" ]]; then
-  echo "[ERROR] Token is not set. Please define it in the add-on options."
+  echo "[ERROR] WIREDOOR_URL is not set. Please define it in the add-on options."
   exit 1
 fi
 
@@ -20,9 +19,5 @@ dnsmasq --server=127.0.0.11 --listen-address=0.0.0.0 --bind-interfaces
 echo "Connecting to ${WIREDOOR_URL}..."
 
 /usr/bin/wiredoor connect --url="${WIREDOOR_URL}" --token="${TOKEN}"
-
-# /usr/bin/wiredoor gateway --subnet "${GATEWAY_SUBNET}"
-
-# /usr/bin/wiredoor http homeassistant --backend
 
 /usr/bin/wiredoor status --watch --interval=10
